d25d762139f900d69e1f5da727e90af0e6d34a3b
hamedfarahikharazmi@gmail.com <hamedfarahikharazmi@gmail.com>
2020-03-02 04:23:59 +0330

    + fix some bugs

--- src/main/java/ir/khu/jaobshaar/service/user/AccountController.java
+++ src/main/java/ir/khu/jaobshaar/service/user/AccountController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @CrossOrigin
 @RequestMapping("/api/account")
-public class AccountController {
+public class 	AccountController {
 
     private final UserManager userManager;
 
