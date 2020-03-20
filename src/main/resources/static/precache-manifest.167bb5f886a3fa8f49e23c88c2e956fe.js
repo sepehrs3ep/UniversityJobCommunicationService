9515e0148103b29d166733c90123c33c5be67ee7
hamedfarahikharazmi@gmail.com <hamedfarahikharazmi@gmail.com>
2020-03-20 19:17:18 +0330

    + add ui files into backend

---
+++ src/main/resources/static/precache-manifest.167bb5f886a3fa8f49e23c88c2e956fe.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa266291ef695fa508b48a3f26d6e5ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "478c8cf75549ddf30ddf",
+    "url": "/static/css/2.3e64e859.chunk.css"
+  },
+  {
+    "revision": "f30e6a0bd9fb48d4cd55",
+    "url": "/static/css/main.c37731f8.chunk.css"
+  },
+  {
+    "revision": "478c8cf75549ddf30ddf",
+    "url": "/static/js/2.e9b12113.chunk.js"
+  },
+  {
+    "revision": "6b77b318e4e3489d11ab17688603ca19",
+    "url": "/static/js/2.e9b12113.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f30e6a0bd9fb48d4cd55",
+    "url": "/static/js/main.1107ee04.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "2f5f6c3a34330871164ed95643afc8f8",
+    "url": "/static/media/1.2f5f6c3a.png"
+  },
+  {
+    "revision": "613766453c16cebcdf48a11fa1040390",
+    "url": "/static/media/3.61376645.png"
+  },
+  {
+    "revision": "a6eec0bece183633fb85d29541c1f2ad",
+    "url": "/static/media/attache-png-6.a6eec0be.png"
+  },
+  {
+    "revision": "c301a4c3dbb4f6ddc62435121952701b",
+    "url": "/static/media/poster.c301a4c3.png"
+  }
+]);
