947ca25e847d7ee36092d33f1f410842e58ef741
hamedfarahikharazmi@gmail.com <hamedfarahikharazmi@gmail.com>
2020-08-12 23:32:59 +0430

    add new ui

---
+++ src/main/resources/static/precache-manifest.3f780e62eaeb2bb28e1c0fe763b19cc0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93a94e5d38d0c6bcb67dcf8c1afc96e4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1e6a432248701112ddfc",
+    "url": "/static/css/2.6173a342.chunk.css"
+  },
+  {
+    "revision": "3e65b5013ddb8c965bc9",
+    "url": "/static/css/main.3c2aeee0.chunk.css"
+  },
+  {
+    "revision": "1e6a432248701112ddfc",
+    "url": "/static/js/2.d45d1690.chunk.js"
+  },
+  {
+    "revision": "6b77b318e4e3489d11ab17688603ca19",
+    "url": "/static/js/2.d45d1690.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e65b5013ddb8c965bc9",
+    "url": "/static/js/main.75fc2b68.chunk.js"
+  },
+  {
+    "revision": "19133a5d6c7be98533aa",
+    "url": "/static/js/runtime-main.107bb8ca.js"
+  },
+  {
+    "revision": "2f5f6c3a34330871164ed95643afc8f8",
+    "url": "/static/media/1.2f5f6c3a.png"
+  },
+  {
+    "revision": "613766453c16cebcdf48a11fa1040390",
+    "url": "/static/media/3.61376645.png"
+  },
+  {
+    "revision": "a6eec0bece183633fb85d29541c1f2ad",
+    "url": "/static/media/attache-png-6.a6eec0be.png"
+  },
+  {
+    "revision": "c301a4c3dbb4f6ddc62435121952701b",
+    "url": "/static/media/poster.c301a4c3.png"
+  }
+]);
